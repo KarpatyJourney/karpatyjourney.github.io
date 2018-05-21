7305d823f422e93d4605acc3b4cfcbd91fc2fb38
KarpatyJourney <karpatyjourney@ukr.net>
2018-05-21 21:11:46 +0300

    Add several hotels in Kreminets

---
+++ js/other-route-points.js
@@ -0,0 +1,37 @@
+// Some other route points in this file
+
+// Hotel "Kremin" in Kreminets
+const hotelKremin = L.marker([50.11898, 25.68718]).addTo(mymap);
+
+hotelKremin.bindPopup(`<h1>Готель “Кремінь”</h1>
+                       <h2>Hotel "Kremin"</h2>
+                       <img class="popup-image" src="images/other-route-points/hotel-kremin.jpg" alt="Hotel Kremin">
+                       <div class="contacts-in-popup">
+                         <span>Tel: <a href="tel:+380953670338">+38 (095) 367-03-38</a></span>
+                         <span>E-mail: <a href="mailto:hotelkremenec@gmail.com">hotelkremenec@gmail.com</a></span>
+                       </div>
+                       <a href="http://www.kremin-hotel.com.ua/hotel.html" target="blank">Детальніше</a>`);
+
+// Hotel "Edem" in Kreminets
+const hotelEdem = L.marker([50.1234129, 25.7166859]).addTo(mymap);
+
+hotelEdem.bindPopup(`<h1>Готель “Едем”</h1>
+                     <h2>Hotel "Edem"</h2>
+                     <img class="popup-image" src="images/other-route-points/hotel-edem-kreminets.jpg" alt="Hotel Edem">
+                     <div class="contacts-in-popup">
+                       <span>Tel: <a href="tel:+380977177248">+38 (097) 717-72-48</a></span>
+                       <span>E-mail: <a href="mailto:otel_edem@ukr.net">otel_edem@ukr.net</a></span>
+                     </div>
+                     <a href="http://kremenets.net/index.html#mini-hotel" target="blank">Детальніше</a>`);
+
+// Hotel "Vika" in Kreminets                  
+const hotelVika = L.marker([50.11102, 25.72576]).addTo(mymap);
+
+hotelVika.bindPopup(`<h1>Готель “Віка”</h1>
+                     <h2>Hotel "Vika"</h2>
+                     <img class="popup-image" src="images/other-route-points/Hotel_Vika_Kremenets_Zdanie.jpg" alt="Hotel Vika">
+                     <div class="contacts-in-popup">
+                       <span>Tel: <a href="tel:+380354623883">+38 (03546) 2-38-83</a></span>
+                       <span>E-mail: <a href="mailto:otel_edem@ukr.net">otel_edem@ukr.net</a></span>
+                     </div>
+                     <a href="http://kremenets.net/index.html#mini-hotel" target="blank">Детальніше</a>`);
