2934850ce6f5081530714f5ee68303e13bbe9d76
KarpatyJourney <karpatyjourney@ukr.net>
2018-06-04 19:52:12 +0300

    Add Vinnitsa & Odessa route points

--- js/other-route-points.js
+++ js/other-route-points.js
@@ -64,3 +64,96 @@ const bobsleighTrack = L.marker([50.102008, 25.701302]).addTo(mymap);
 bobsleighTrack.bindPopup(`<h1>Санно-бобслейна траса</h1>
                           <h2>Bobsleigh track</h2>
                           <img class="popup-image" src="images/other-route-points/bobsleigh-track-kreminets.jpg" alt="Bobsleigh track">`);
+
+
+// Points in Vinnitsa
+
+// Flat on Kyletska street Vinnitsa
+const flatKyletskaVinnitsa = L.marker([49.226759, 28.4423]).addTo(mymap);
+
+flatKyletskaVinnitsa.bindPopup(`<h1>1-кімнатна квартира у Вінниці. Келецька 12А</h1>
+                                <h2>Flat on Kyletska 12A street</h2>
+                                <p>Ціна: 380.00 грн/доба</p>
+                                <p>Price: 380.00 grn/day</p>
+                                <div class="contacts-in-popup">
+                                  <span>Tel: <a href="tel:+380988774290">+38 (098) 877-42-90</a></span>
+                                  <span>Tel: <a href="tel:+380505464677">+38 (050) 546-46-77</a></span>
+                                  <span>Tel: <a href="tel:+380638313876">+38 (063) 831-38-76</a></span>
+                                </div>
+                                <a href="http://orendar.com/f15005/" target="blank">Детальніше</a>`);
+
+// Flat on Malynovskogo street Vinnitsa
+const flatMalynovskogoVinnitsa = L.marker([49.228076, 28.457835]).addTo(mymap);
+
+flatMalynovskogoVinnitsa.bindPopup(`<h1>2-кімнатна квартира у Вінниці. Малиновського 23</h1>
+                                    <h2>Flat on Malynovskogo 23 street</h2>
+                                    <p>Ціна: 350.00 грн/доба</p>
+                                    <p>Price: 350.00 grn/day</p>
+                                    <div class="contacts-in-popup">
+                                      <span>Tel: <a href="tel:+380936797577">+38 (093) 679-75-77</a></span>
+                                      <span>Tel: <a href="tel:+380960806769">+38 (096) 080-67-69</a></span>
+                                    </div>
+                                    <a href="http://orendar.com/f40/" target="blank">Детальніше</a>`);
+
+// Flat on Pyrogova street Vinnitsa
+const flatPyrogovaVinnitsa = L.marker([49.229744, 28.45393]).addTo(mymap);
+
+flatPyrogovaVinnitsa.bindPopup(`<h1>1-кімнатна квартира у Вінниці. Пирогова 23</h1>
+                                <h2>Flat on Pyrogova 23 street</h2>
+                                <p>Ціна: 350.00 грн/доба</p>
+                                <p>Price: 350.00 grn/day</p>
+                                <div class="contacts-in-popup">
+                                  <span>Tel: <a href="tel:+380677914006">+38 (067) 791-40-06</a></span>
+                                  <span>Tel: <a href="tel:+380633020485">+38 (063) 302-04-85</a></span>
+                                </div>
+                                <a href="http://orendar.com/f6496/" target="blank">Детальніше</a>`);
+
+
+// Points in Odessa
+
+// Flat on Mala Arnautska street Odessa
+const flatSmallArnautskayaOdessa = L.marker([46.470789, 30.747739]).addTo(mymap);
+
+flatSmallArnautskayaOdessa.bindPopup(`<h1>1-кімнатна квартира у Одесі. Мала Арнаутська 24</h1>
+<h2>Flat on Mala Arnautska 24 street</h2>
+<p>Ціна: 409.00 грн/доба</p>
+<p>Price: 409.00 grn/day</p>
+<a href="https://www.dobovo.com/ru/%D0%BE%D0%B4%D0%B5%D1%81%D1%81%D0%B0-%D0%BA%D0%B2%D0%B0%D1%80%D1%82%D0%B8%D1%80%D1%8B-%D0%BF%D0%BE%D1%81%D1%83%D1%82%D0%BE%D1%87%D0%BD%D0%BE/%D0%BA%D0%B2%D0%B0%D1%80%D1%82%D0%B8%D1%80%D0%B0-%D0%B2-%D1%86%D0%B5%D0%BD%D1%82%D1%80%D0%B5-%D0%BE%D0%B4%D0%B5%D1%81%D1%81%D1%8B-36969.html" target="blank">Детальніше</a>`);
+
+// Flat on Zhukovskogo street Odessa
+const flatZhukovskogoOdessa = L.marker([46.479312, 30.741693]).addTo(mymap);
+
+flatZhukovskogoOdessa.bindPopup(`<h1>2-3-кімнатна квартира у Одесі. Жуковського 15</h1>
+                                 <h2>Flat on Zhukovskogo 15 street</h2>
+                                 <p>Ціна: 600.00 грн/доба</p>
+                                 <p>Price: 600.00 grn/day</p>
+                                 <div class="contacts-in-popup">
+                                   <span>Tel: <a href="tel:+380672204495">+38 (067) 220-44-95</a></span>
+                                 </div>
+                                 <a href="http://orendar.com/f4571/" target="blank">Детальніше</a>`);
+
+// Flat on Grecheskaya street Odessa
+const flatGrecheskayaOdessa = L.marker([46.482725, 30.73944]).addTo(mymap);
+
+flatGrecheskayaOdessa.bindPopup(`<h1>1-кімнатна квартира у Одесі. Грецька 30</h1>
+                                 <h2>Flat on Grecheskaya 30 street</h2>
+                                 <p>Ціна: 600.00 грн/доба</p>
+                                 <p>Price: 600.00 grn/day</p>
+                                 <div class="contacts-in-popup">
+                                   <span>Tel: <a href="tel:+380674863888">+38 (067) 486-38-88</a></span>
+                                   <span>Tel: <a href="tel:+380636074262">+38 (063) 607-42-62</a></span>
+                                   <span>Skype: <a href="skype:odessa888.com">odessa888.com</a></span>
+                                 </div>
+                                 <a href="http://orendar.com/f20223/" target="blank">Детальніше</a>`);
+
+// Flat on Derybasovskaya street Odessa
+const flatDerybasovskayaOdessa = L.marker([46.484369, 30.736817]).addTo(mymap);
+
+flatDerybasovskayaOdessa.bindPopup(`<h1>1-кімнатна квартира у Одесі. Дерибасівська 18</h1>
+                                    <h2>Flat on Derybasovskaya 18 street</h2>
+                                    <p>Ціна: 600.00 грн/доба</p>
+                                    <p>Price: 600.00 grn/day</p>
+                                    <div class="contacts-in-popup">
+                                      <span>Tel: <a href="tel:+380677711055">+38 (067) 771-10-55</a></span>
+                                    </div>
+                                    <a href="http://orendar.com/f11775/" target="blank">Детальніше</a>`);
